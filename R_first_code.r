ed7c9156d4e8b2cdb313559f50a8a22fc546a49b
marharytablaha <92572844+marharytablaha@users.noreply.github.com>
2021-10-22 10:55:08 +0200

    Update R_first_code.r

--- R_first_code.r
+++ R_first_code.r
@@ -12,3 +12,34 @@ fishes
 # plot the diversity of fishes (y) versus the amount of water (x)
 # a function is used with arguments inside!
 plot(water, fishes)
+
+# the data we developed can be stored in a table
+# a table in R is called data frame
+
+streams <- data.frame(water,fishes)
+
+# From now on, we are going to import and or export data
+
+# setwd for Windows
+setwd("C:/lab/")
+
+# Let's export our table!
+write.table(streams, file="my_first_table.txt")
+
+# Some colleagues sent us a table. How to import it in R?
+read.table("my_first_table.txt")
+
+# Assign the table to a name
+margotable <- read.table("my_first_table.txt")
+margotable
+
+# the first statistics for lazy beautiful people
+summary(margotable)
+
+# Marta does not like water
+# Marta wants to get info only on fishes
+summary(margotable$fishes)
+
+# histogram
+hist(margotable$fishes)
+hist(margotable$water)
