42df5a44e61c166a2ce2fee0b255efca28c55c26
marharytablaha <92572844+marharytablaha@users.noreply.github.com>
2021-10-29 11:01:48 +0200

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -6,3 +6,19 @@
 install.packages("raster")
 
 library(raster)
+
+setwd("C:/lab/")
+
+brick("p224r63_2011.grd")
+
+l2011 <- brick("p224r63_2011.grd")
+
+#B1 is the reflectance in the blue wavelenght
+#B2 is the reflectance in the green wavelenght
+#B3 is the reflectance in the red wavelenght
+
+plot(l2011)
+
+cl <- colorRampPalette(c("black","grey","light grey"))(100)
+plot(ll011, col=cl)
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
