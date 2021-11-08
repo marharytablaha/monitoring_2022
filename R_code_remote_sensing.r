45566f21cee5844f0fa70d7bcee076c2ab7b0321
marharytablaha <92572844+marharytablaha@users.noreply.github.com>
2021-11-08 10:56:16 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -58,3 +58,41 @@ plot(l2011$B1_sre, col=clb)
 plot(l2011$B2_sre, col=clg)
 
 # dev.off() is a function that shuts down the device
+
+library(raster)
+l2011<-brick("p224r63_2011.grd")
+
+#---------day 3
+
+# plot the blue band using a blue colorRampPalette
+clb<-colorRampPalette(c("dark blue", "blue", "light blue"))(100)
+plot(l2011$B1_sre, col=clb)
+
+# plot the green band using a green colorRampPalette
+clg<-colorRampPalette(c("dark green", "green", "light green"))(100)
+plot(l2011$B2_sre, col=clg)
+
+# plot multiframe
+par(mfrow=c(1,2))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+#Plot the 2x2 multiframe
+clr<-colorRampPalette(c("dark red", "red", "pink"))(100)
+clnir<-colorRampPalette(c("red", "orange", "yellow"))(100)
+par(mfrow=c(2,2))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+plot(l2011$B3_sre, col=clr)
+plot(l2011$B4_sre, col=clnir)
+
+dev.off()
+
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+
+# plot in false colors, near infrared
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+# plot the infrared in the green
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin")
+# plot the infrared in the blue
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")
