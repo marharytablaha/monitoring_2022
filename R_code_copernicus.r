7107290c938dfe762a8b08b9e45e1acdc61b4dc2
marharytablaha <92572844+marharytablaha@users.noreply.github.com>
2021-12-16 12:14:51 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -1,7 +1,15 @@
 # R code for unloading and visualizing Copernicus data in R
 library(ncdf4)
 library(raster)
+library(RStoolbox)
+library(ggplot2)
+library(viridis)
+
 # Set the working directory
 setwd("C:/lab/Copernicus/")
 vegetation2020<-raster("c_gls_NDVI_202006210000_GLOBE_PROBAV_V3.0.1.nc")
 plot(vegetation2020)
+ggplot() + geom_raster(vegetation2020, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km))
+ggplot() + geom_raster(vegetation2020, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km))+scale_fill_viridis(option="cividis")
+
+
