5e7b615bf9c0b8b6a961c0179158639493039648
marharytablaha <92572844+marharytablaha@users.noreply.github.com>
2021-12-17 10:25:36 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -12,4 +12,30 @@ plot(vegetation2020)
 ggplot() + geom_raster(vegetation2020, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km))
 ggplot() + geom_raster(vegetation2020, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km))+scale_fill_viridis(option="cividis")
 
+##### MAKING COMPARISONS 
 
+rlist <- list.files(pattern="NDVI")
+rlist
+list_rast <- lapply(rlist, raster)
+list_rast
+vegetationstack <- stack(list_rast)
+vegetationstack
+vegetation1999 <- vegetationstack$Normalized.Difference.Vegetation.Index.1km.1
+vegetation2020 <- vegetationstack$Normalized.Difference.Vegetation.Index.1km.2
+
+ggplot() + geom_raster(vegetation2020, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.2)) + scale_fill_viridis(option="viridis") + ggtitle ("NDVI 2020")
+ggplot() + geom_raster(vegetation1999, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.1)) + scale_fill_viridis(option="viridis") + ggtitle ("NDVI 1999")
+P1999 <- ggplot() + geom_raster(vegetation1999, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.1)) + scale_fill_viridis(option="viridis") + ggtitle ("NDVI 1999")
+P2020 <- ggplot() + geom_raster(vegetation2020, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.2)) + scale_fill_viridis(option="viridis") + ggtitle ("NDVI 2020")
+library(patchwork)
+P1999/P2020
+
+# you can crop your image on a certain area, for example Malaysia-Indonesia
+# longitude from 90 to 120
+# latitude from -20 to +20
+ext<-c(90, 120, -20, 20)
+vegetation1999_Malaysia<-crop(vegetation1999,ext)
+vegetation2020_Malaysia<-crop(vegetation2020,ext)
+P1999_Malaysia <- ggplot() + geom_raster(vegetation1999_Malaysia, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.1)) + scale_fill_viridis(option="viridis") + ggtitle ("NDVI 1999")
+P2020_Malaysia <- ggplot() + geom_raster(vegetation2020_Malaysia, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.2)) + scale_fill_viridis(option="viridis") + ggtitle ("NDVI 2020")
+P1999_Malaysia/P2020_Malaysia
