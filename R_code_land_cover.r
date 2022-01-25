8603c3b0f7e24b5d95cee7042f919ba31edc8143
marharytablaha <92572844+marharytablaha@users.noreply.github.com>
2022-01-26 00:28:34 +0100

    Create R_code_land_cover.r

---
+++ R_code_land_cover.r
@@ -0,0 +1,57 @@
+library(raster)
+library(patchwork)
+setwd("D:/Users/marha/Documents/_UNIBO_/ME")
+LC2015<-brick("E100N20_PROBAV_LC100_global_v3.0.1_2015-base_Tree-CoverFraction-layer_EPSG-4326.tif")
+LC2019<-brick("E100N20_PROBAV_LC100_global_v3.0.1_2019-nrt_Tree-CoverFraction-layer_EPSG-4326.tif")
+
+# recall the variables to read the descriptors
+
+# let's plot the images
+plot(LC2015)
+plot(LC2019)
+
+# crop
+ext<-c(99.6419, 119.2758, 0.8527, 7.3529)
+LC2015<-crop(LC2015,ext)
+LC2019<-crop(LC2019,ext)
+
+# multiframe
+par(mfrow=c(1,2))
+plot(LC2015)
+plot(LC2019)
+
+# let's change the palette
+library(viridis)
+viridis(3) #generate a palette
+viridis<-colorRampPalette(c("#440154FF", "#21908CFF", "#FDE725FF"))(100) 
+
+par(mfrow=c(2,1))
+plot(LC2015, col=viridis)
+plot(LC2019, col=viridis)
+
+library(RStoolbox)
+LC2015c<-unsuperClass(LC2015, nClasses=2)
+plot(LC2015c$map)
+# value 1 = urban and crop area
+# value 2 = forest
+freq(LC2015c$map)
+
+# result
+#      value    count
+# [1,]     1 35620029
+# [2,]     2  9980411
+# [3,]    NA 81704920
+
+total<-35620029+9980411
+propurbanagri<-9980411/total
+propforest<-35620029/total
+# propforest = 0.7811334 ~ 0.78
+# propurbanagri = 0.2188666 ~ 0.22
+
+# build a dataframe
+cover<-c("Forest", "Urban and Agriculture")
+prop2015<-c(propforest, propurbanagri)
+proportion2015<-data.frame(cover, prop2015)
+
+library(ggplot2)
+ggplot(proportion2015, aes(x=cover, y=prop2015, color=cover)) + geom_bar(stat="identity", fill="white")
